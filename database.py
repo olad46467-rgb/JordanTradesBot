0e3e859591cb0b79de2cc8875f5bde06e3d9af1b
olad46467-rgb <olad46467@gmail.com>
2026-07-03 16:07:04 -0700

    Update database.py

--- database.py
+++ database.py
@@ -4,23 +4,28 @@ conn = sqlite3.connect("users.db")
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY
+CREATE TABLE IF NOT EXISTS users(
+    user_id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    username TEXT
 )
 """)
 
 conn.commit()
 
 
-def add_user(user_id):
+def add_user(user_id, first_name="", username=""):
     cursor.execute(
-        "INSERT OR IGNORE INTO users (user_id) VALUES (?)",
-        (user_id,)
+        """
+        INSERT OR IGNORE INTO users(user_id, first_name, username)
+        VALUES(?,?,?)
+        """,
+        (user_id, first_name, username),
     )
     conn.commit()
 
 
-def get_users():
+def get_all_users():
     cursor.execute("SELECT user_id FROM users")
     return cursor.fetchall()
 

ebbd18df3ea87fa2029313a160d9ddd889e09da1
olad46467-rgb <olad46467@gmail.com>
2026-07-03 16:06:15 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))

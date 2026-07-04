1c6530ed5da8cb11c258fd126de3c1d06a87c001
olad46467-rgb <olad46467@gmail.com>
2026-07-04 06:31:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import asyncio
+
 from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
 from config import BOT_TOKEN
 from handlers.start import router as start_router
 from handlers.admin import router as admin_router
-import asyncio
+
 
 async def main():
     bot = Bot(
@@ -17,9 +20,10 @@ async def main():
     dp.include_router(start_router)
     dp.include_router(admin_router)
 
-    print("✅ JordanTradesBot Started Successfully!")
+    print("✅ JordanTradesBot is running...")
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())

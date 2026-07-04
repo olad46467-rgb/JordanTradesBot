4e8367437e00ae0b9e2b94d2dfba467f0169da5f
olad46467-rgb <olad46467@gmail.com>
2026-07-04 06:47:59 -0700

    Update forcejoin.py

--- forcejoin.py
+++ forcejoin.py
@@ -1,9 +1,18 @@
+from aiogram.enums import ChatMemberStatus
 from aiogram.exceptions import TelegramBadRequest
+
 from config import CHANNEL_USERNAME
+
 
 async def is_user_joined(bot, user_id):
     try:
         member = await bot.get_chat_member(CHANNEL_USERNAME, user_id)
-        return member.status in ("member", "administrator", "creator")
+
+        return member.status in (
+            ChatMemberStatus.MEMBER,
+            ChatMemberStatus.ADMINISTRATOR,
+            ChatMemberStatus.OWNER,
+        )
+
     except TelegramBadRequest:
         return False
